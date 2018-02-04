09e58c104e0d7b8f32463c51b088bb438d55bc45
sbk台式 <444503829@qq.com>
2018-02-04 21:41:46 +0800

    个人中心的分页

--- ChuangKeWeb/Content/Script/Person/pserson.js
+++ ChuangKeWeb/Content/Script/Person/pserson.js
@@ -1,1 +1,15 @@
-﻿
+﻿function changefirstli(){
+    $('.first-li').css("display","block");
+    $('.second-li').css("display","none");
+    $('.third-li').css("display","none");    
+}
+function changesecondli(){
+    $('.first-li').css("display","none");
+    $('.second-li').css("display","block");
+    $('.third-li').css("display","none");    
+}
+function changethirdli(){
+    $('.first-li').css("display","none");
+    $('.second-li').css("display","none");
+    $('.third-li').css("display","block");  
+}
