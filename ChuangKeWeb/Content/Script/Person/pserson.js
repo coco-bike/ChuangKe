d0dc388edaf491a532290f54f44bf451a5f76693
hihi_zh <444503829@qq.com>
2018-02-09 18:18:04 +0800

    主页和报名页面的UI修改

--- ChuangKeWeb/Content/Script/Person/pserson.js
+++ ChuangKeWeb/Content/Script/Person/pserson.js
@@ -22,3 +22,27 @@ function changefourthli(){
     $('.third-li').css("display","none");
     $('.forth-li').css("display","block");    
 }
+
+//上传图片的插件初始化
+$("#file-1").fileinput({
+    uploadUrl: "../../admin/home/SaveUploadedFile", // you must set a valid URL here else you will get an error
+    allowedFileExtensions: ['jpg', 'png', 'gif'],
+    overwriteInitial: false,
+    maxFileSize: 1000,
+    minFileCount: 1,
+    maxFileCount: 1,
+    language: "zh",
+    validateInitialCount: true,
+    elCaptionText: "上传一张作品封面",
+    //allowedFileTypes: ['image', 'video', 'flash'],
+    msgFilesTooMany: "选择上传的文件数量({n}) 超过允许的最大数值{m}！",
+    slugCallback: function (filename) {
+        return filename.replace('(', '_').replace(']', '_');
+    },
+});
+
+//初始化文本编辑器
+$(function () {
+    //初始化
+    var ue = UE.getEditor('container');
+});
